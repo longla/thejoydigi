43ca038d7b1a4624a5aaf1fb32d74ce7c80df906
LONG LA <baolonguit@gmail.com>
2025-03-31 10:10:29 -0700

    Update Microsoft Clarity script with new tracking ID qwx9hqufpk

--- src/components/meta-data.tsx
+++ src/components/meta-data.tsx
@@ -54,6 +54,20 @@ export const DefaultMetaData: React.FC<MetaDataProps> = ({
         }}
       />
       {/* End Google Analytics */}
+
+      {/* Microsoft Clarity */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "qwx9hqufpk");
+          `,
+        }}
+      />
+      {/* End Microsoft Clarity */}
     </Head>
   );
 };
